ab6fedb5e228713badcf79e0fc8977b109d03a39
oranssy <oranssy123@gmail.com>
2022-12-09 11:32:20 +0900

    221209
    nmnmnmnm

--- assets/js/section2.js
+++ assets/js/section2.js
@@ -1,22 +1,10 @@
-// section2 : personality 마우스 오버시 내용 띄우기
-
-// const Select1 = document.querySelector(".select01");
-
-// Select1.addEventListener("mouseenter", () => {
-//     Select1.style.cursor = "cell";
-//     Select1.style.color = "#8c89b0";
-//     Intro1.style.display = "block";
-// });
-// Select1.addEventListener("mouseleave", () => {
-//     Select1.style.color = "#ffffff";
-//     Intro1.style.display = "none";
-// });
+// 마우스 오버 시 내용 띄우기
 
 const { gsap } = window;
 
 gsap.timeline()
-    .set(".person", { autoAlpha: 1 })
-    .from(".person__item-innerTitle", {
+    .set(".person__item", { autoAlpha: 1 })       // .person
+    .from(".person__item-innertext", {
         delay: 1,
         duration: 0.85,
         xPercent: 25,
@@ -32,26 +20,26 @@ gsap.timeline()
         ease: "expo.out",
     });
 
-    const menuItems = document.querySelectorAll(".person__item");
+    const personItems = document.querySelectorAll(".person__item");
 
-    menuItems.forEach( (item) => {
-        const imageWrapper = item.querySelector(".person__item-image_wrap");
-        const imageWrapperBounds = imageWrapper.getBoundingClientRect();
+    personItems.forEach( (item) => {
+        const imageWrap = item.querySelector(".person__item-image_wrap");
+        const imageWrapBounds = imageWrap.getBoundingClientRect();
 
         let itemBounds = item.getBoundingClientRect();
 
         const onMouseEnter = () => {
-            gsap.set(imageWrapper, {
+            gsap.set(imageWrap, {
                 scale: 0.8,
                 xPercent: 25,
                 yPercent: 50,
                 rotation: -15,
             });
-            gsap.to(imageWrapper, { opacity: 1, scale: 1, yPercent: 0, rotation: 0 });
+            gsap.to(imageWrap, { opacity: 1, scale: 1, yPercent: 0, rotation: 0 });
         };
 
         const onMouseLeave = () => {
-            gsap.to(imageWrapper, {
+            gsap.to(imageWrap, {
                 opacity: 0,
                 yPercent: -50,
                 xPercent: 25,
@@ -61,13 +49,13 @@ gsap.timeline()
         };
 
         const onMouseMove = ({ x, y }) => {
-            let yOffset = itemBounds.top / imageWrapperBounds.height;
+            let yOffset = itemBounds.top / imageWrapBounds.height;
             yOffset = gsap.utils.mapRange(0, 1.5, -150, 150, yOffset);
 
-            gsap.to(imageWrapper, {
+            gsap.to(imageWrap, {
                 duration: 1.25,
-                x: Math.abs(x - itemBounds.left) - imageWrapperBounds.width / 1.55,
-                y: Math.abs(y - itemBounds.top) - imageWrapperBounds.height / 2 - yOffset,
+                x: Math.abs(x - itemBounds.left) - imageWrapBounds.width / 1.55,
+                y: Math.abs(y - itemBounds.top) - imageWrapBounds.height / 2 - yOffset,
             });
         };
 
